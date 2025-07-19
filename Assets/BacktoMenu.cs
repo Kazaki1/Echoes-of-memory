110aa0bc24229bde305b04e01ed06695697a61a0
Võ Thành Luân <2331540274@vaa.edu.vn>
2025-07-19 09:47:49 +0700

    Audio

---
+++ Assets/BacktoMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour
+{
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ Assets/Script/AudioManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [Header("Audio Sources")]
+    public AudioSource bgmAudioSource;  // Nhạc nền
+    public AudioSource vfxAudioSource;  // Âm thanh hiệu ứng (VFX: obstacle, heal...)
+
+    [Header("Audio Clips")]
+    public AudioClip backgroundMusic;   // File nhạc nền
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // Tự động phát nhạc nền nếu được gán
+        if (bgmAudioSource != null && backgroundMusic != null)
+        {
+            bgmAudioSource.clip = backgroundMusic;
+            bgmAudioSource.loop = true;
+            bgmAudioSource.Play();
+        }
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        if (vfxAudioSource != null && clip != null)
+        {
+            vfxAudioSource.PlayOneShot(clip);
+        }
+    }
+
+    public void PlayBGM(AudioClip clip)
+    {
+        if (bgmAudioSource != null && clip != null)
+        {
+            bgmAudioSource.clip = clip;
+            bgmAudioSource.loop = true;
+            bgmAudioSource.Play();
+        }
+    }
+
+    public void StopBGM()
+    {
+        if (bgmAudioSource != null)
+        {
+            bgmAudioSource.Stop();
+        }
+    }
+}
